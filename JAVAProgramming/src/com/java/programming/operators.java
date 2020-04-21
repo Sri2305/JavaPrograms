816687de6e78e3b7428c3b6012841653e672e3b9
Sri2305 <lankalasrilakshmi@gmail.com>
2020-04-21 23:06:56 +0200

    Latest changes

---
+++ JAVAProgramming/src/com/java/programming/operators.java
@@ -0,0 +1,21 @@
+package com.java.programming;
+
+public class operators {
+
+	public static void main(String[] args) {
+		
+		int i=1;		
+		int a=++i;
+		int j=i++;
+		System.out.println(a);//2
+		System.out.println(i);//3
+		System.out.println(j);//2 
+				
+		int x=1;
+		int y=--x;
+		int z= x--;
+		System.out.println(x);//-1
+		System.out.println(y);//0
+		System.out.println(z);//0
+	}
+}
