5da44fe0a78b9fee9158f8c038a33d92038fb72a
Srilakshmi Lankala <54366225+Sri2305@users.noreply.github.com>
2025-05-27 17:42:12 +0200

    Update pyramids.java

--- JAVAProgramming/src/com/java/programming/basics/pyramids.java
+++ JAVAProgramming/src/com/java/programming/basics/pyramids.java
@@ -1,1 +1,17 @@
 
+package basics;
+
+public class loops {
+
+	public static void main(String[] args) {
+	int k=15;
+		for(int i=0;i<=4;i++) {
+			for(int j=0;j<=4-i;j++) {
+				k--;
+				System.out.print(k+ "\t");
+			}
+			System.out.println();
+		}
+	}
+	
+}
